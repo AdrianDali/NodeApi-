041df891ea376a54b32842771bc6a3e7fda8e333
AdrianDali <hadriandali@gmail.com>
2021-12-20 12:49:46 -0600

    Avance en la API

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const faker = require('faker');
+//const faker = require('faker');
+//const id = require('faker/lib/locales/id_ID');
+const ProductsService = require('./../services/product.service')
 
 const router = express.Router();
+
+const service = new ProductsService();
 //---------------------------------------------------------------
 router.get('/', (req,res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for(let index = 0; index < limit; index++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.json(products)
 })
 /**
@@ -40,19 +35,48 @@ router.get('/filter', (req,res) => {
 // los dos puntos significa que es un parametro
 router.get('/:id', (req,res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  })
-})
+  const product = service.findOne(id)
+  res.json(product)
+  //if( id === '999'){
+    //res.status(404).json({
+      //message: "not found"
+    //})
+  //}else{
+    //res.json({
+      //id,
+      //name: 'Product 2',
+      //price: 2000
+    //})
+ // }
+
+}
+)
 //-----------------------------------------------------------------
 router.post('/', (req,res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   })
  })
+//---------------------------------------------------------------------
+router.patch('/:id', (req,res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  })
+ })
+//---------------------------------------------------------------------
+router.delete('/:id',(req,res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id,
+  })
+})
+
 
 module.exports = router;
